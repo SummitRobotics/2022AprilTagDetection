95d428e686abd0705cca16a578d614ec37fa26a6
Owen McCormick <oweninoregon@gmail.com>
2022-11-29 16:57:52 -0800

    Changes to make work with webcam

--- detectAprilTag.py
+++ detectAprilTag.py
@@ -5,13 +5,28 @@ import numpy as np
 options = apriltag.DetectorOptions(families='tag36h11')
 detector = apriltag.Detector(options)
 
-def runPipeline(inputImage, llrobot):
+feed = cv2.VideoCapture(0)
+
+if not feed.isOpened():
+	print("Cannot open camera.")
+	exit()
+
+while True: 
+
+	ret, frame = feed.read()
+
+	if not ret:
+		print("Can't receive frame")
+		break
+
 	# TODO: Implement llpython
 	llpython = [1,1,1,1,1,1,1,1]
 
 	# Load the image and convert it to grayscale
 	# print("[INFO] loading image...")
-	image = cv2.imread(inputImage)
+	cv2.imshow("Frame", frame)
+	
+	image = cv2.imread(frame)
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 	# Detect AprilTags in the input image
@@ -47,11 +62,14 @@ def runPipeline(inputImage, llrobot):
 		# print("[INFO] tag family: {}".format(tagFamily))
 
 		# figure out contours?
-		contour = np.array([[ptA[0], ptA[1]], [ptB[0], ptB[1]], [ptC[0], ptC[1]], [ptD[0], ptD[1]]], dtype = np.int32)
-		contourArea = abs((ptA[0] - ptD[0])) * abs((ptA[1] - ptD[1]))
+		# contour = np.array([[ptA[0], ptA[1]], [ptB[0], ptB[1]], [ptC[0], ptC[1]], [ptD[0], ptD[1]]], dtype = np.int32)
+		# contourArea = abs((ptA[0] - ptD[0])) * abs((ptA[1] - ptD[1]))
 
-		if contourArea > largestArea:
-			largestArea = contourArea
-			returnContour = contour
+		# if contourArea > largestArea:
+			# largestArea = contourArea
+			# returnContour = contour
 
-	return returnContour, image, llpython
+	if cv2.waitKey(1) & 0xFF == ord('q'): break
+
+feed.release()
+cv2.destroyAllWindows()
